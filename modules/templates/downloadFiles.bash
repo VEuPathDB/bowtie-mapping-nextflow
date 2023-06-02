0602a4640e082706d2bfd95d04df5e6ae4659717
rdemko2332 <rdemko2332@gmail.com>
2023-06-02 10:37:42 -0400

    Create home dir

--- modules/templates/downloadFiles.bash
+++ modules/templates/downloadFiles.bash
@@ -7,6 +7,7 @@ if [ -s $HOME/.ncbi/user-settings.mkfg ]
 then
     fasterq-dump --split-3 ${id}
 else
+    mkdir -p $HOME
     cp /usr/bin/user-settings.mkfg $HOME/.ncbi/
     fasterq-dump --split-3 ${id}
 fi
