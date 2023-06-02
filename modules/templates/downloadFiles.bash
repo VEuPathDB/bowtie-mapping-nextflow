494be2a7bc18e2c56f69a75a29e4a2cc6df83d73
rdemko2332 <rdemko2332@gmail.com>
2023-06-02 10:22:07 -0400

    Resolving config file issue

--- modules/templates/downloadFiles.bash
+++ modules/templates/downloadFiles.bash
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-fasterq-dump --split-3 ${id}
+
+echo $HOME
+if [ -s $HOME/.ncbi/user-settings.mkfg ]
+then
+    fasterq-dump --split-3 ${id}
+else
+    cp /root/.ncbi/user-settings/mkfg $HOME/.ncbi/user-setting.mkfg
+    fasterq-dump --split-3 ${id}
+fi
